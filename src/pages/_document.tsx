56a3de1a2fef90de1ac37b674bc16bc27dc5498f
Jarrod Watts <jarrodwatts16@gmail.com>
2023-11-27 17:26:53 +1100

    Add chat gpt generated meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,22 @@ export default function Document() {
       <Head />
       <meta name="viewport" content="width=device-width, user-scalable=no" />
       <meta name="theme-color" content="#ffffff" />
+      <meta
+        name="description"
+        content="Wallaby Pay - Send cryptocurrency to one another with ease."
+      />
+      <meta
+        name="keywords"
+        content="Wallaby Pay, cryptocurrency, send cryptocurrency, digital payments"
+      />
+      <meta name="author" content="Jarrod Watts" />
+      <meta name="og:title" content="Wallaby Pay" />
+      <meta
+        name="og:description"
+        content="A secure and user-friendly app for sending cryptocurrency."
+      />
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content="https://www.wallabypay.com"></meta>
       <body>
         <Main />
         <NextScript />
