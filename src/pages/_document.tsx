65647a3fe956ceec6397b0a78f6ab6f1a18ee428
Jarrod Watts <jarrodwatts16@gmail.com>
2023-11-27 17:12:45 +1100

    Add some meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="viewport" content="width=device-width, user-scalable=no" />
+      <meta name="theme-color" content="#ffffff" />
       <body>
         <Main />
         <NextScript />
