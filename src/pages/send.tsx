43c40cfb9a2fb44fe6af9cbec8d907b78d6ef98d
Jarrod Watts <jarrodwatts16@gmail.com>
2023-11-27 16:09:50 +1100

    Add Lens profile method

--- src/pages/send.tsx
+++ src/pages/send.tsx
@@ -1,4 +1,5 @@
 import {
+  MediaRenderer,
   NATIVE_TOKEN_ADDRESS,
   useAddress,
   useBalance,
@@ -9,32 +10,28 @@ import AppContainer from "@/components/AppContainer";
 import { useState } from "react";
 import { Separator } from "@/components/ui/separator";
 import { Card } from "@/components/ui/card";
-import { BookUser, QrCode, Sprout } from "lucide-react";
+import { BookUser, ChevronLeft, Sprout } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import formatNumber from "@/lib/numberFormatter";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { CHAIN } from "@/const/config";
-import { Skeleton } from "@/components/ui/skeleton";
 import Link from "next/link";
 import { TransactionReceipt } from "@ethersproject/providers";
 import { useRouter } from "next/router";
+import { useDebounce } from "use-debounce";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Profile, useSearchProfiles } from "@lens-protocol/react-web";
 
 const formPhases = [
   {
     title: "Who are you paying?",
-    description:
-      "Enter the wallet address of the person you want to send money to.",
+    description: "Who's the lucky recipient?",
     options: [
       {
         key: "address",
         title: "Wallet Address",
         icon: <BookUser />,
-      },
-      {
-        key: "qr",
-        title: "QR Code",
-        icon: <QrCode />,
       },
       {
         key: "lens",
@@ -62,8 +59,18 @@ export default function SendPage() {
   const router = useRouter();
 
   // Phase 0 State
+  const [enterAddressOption, setEnterAddressOption] = useState<
+    null | "address" | "lens"
+  >(null);
   const [addressToPay, setAddressToPay] = useState<string>("");
   const [addressToPayError, setAddressToPayError] = useState<string>("");
+
+  const [lensProfileToPay, setLensProfileToPay] = useState<string>("");
+  const [debouncedLensProfile] = useDebounce(lensProfileToPay, 1000);
+  const { data: profiles, loading: loadingProfiles } = useSearchProfiles({
+    query: debouncedLensProfile,
+  });
+  const [selectedLensProfile, setSelectedLensProfile] = useState<Profile>();
 
   // Phase 1 State
   const [amountToPay, setAmountToPay] = useState<string>("");
@@ -80,9 +87,6 @@ export default function SendPage() {
     useBalance(NATIVE_TOKEN_ADDRESS);
 
   const [formPhase, setFormPhase] = useState<number>(0);
-  const [enterAddressOption, setEnterAddressOption] = useState<
-    null | "address" | "qr" | "lens"
-  >(null);
 
   const sdk = useSDK();
   async function sendPayment() {
@@ -108,6 +112,31 @@ export default function SendPage() {
     }
   }
 
+  function validateAddress(addressToSend: string): string | true {
+    if (!addressToSend) {
+      return "Please enter an address";
+    }
+
+    if (addressToSend === address) {
+      return "You can't pay yourself";
+    }
+
+    if (addressToSend.length !== 42) {
+      return "Invalid address";
+    }
+
+    if (!addressToSend.startsWith("0x")) {
+      return "Invalid address";
+    }
+
+    return true;
+  }
+
+  // Render back button if we're not on the first or last phase
+  const shouldRenderBackButton =
+    (formPhase > 0 || (formPhase === 0 && enterAddressOption !== null)) &&
+    formPhase !== formPhases.length - 1;
+
   if (!address) {
     return (
       <AppContainer>
@@ -125,12 +154,35 @@ export default function SendPage() {
           Pay Someone
         </h1>
         <Separator className="w-5/6  mt-4 lg:mt-8 mb-8" />
-        <h2 className="text-xl lg:text-2xl font-semibold text-center lg:text-start">
-          {formPhases[formPhase].title}
-        </h2>
-        <p className="text-sm lg:text-md text-muted-foreground lg:mt-2 lg:mb-4 max-w-xs lg:max-w-2xl leading-normal text-center lg:text-start">
-          {formPhases[formPhase].description}
-        </p>
+
+        <div className="flex flex-col-reverse lg:flex-row gap-8 lg:gap-2 items-center justify-center lg:w-full lg:justify-between">
+          <h2 className="text-xl lg:text-2xl font-semibold text-center lg:text-start lg:mb-4">
+            {formPhases[formPhase].title}
+          </h2>
+
+          {shouldRenderBackButton && (
+            <Button
+              variant="outline"
+              onClick={() => {
+                if (formPhase === 0 && enterAddressOption) {
+                  setEnterAddressOption(null);
+                }
+
+                if (formPhase === 1) {
+                  setEnterAddressOption(null);
+                }
+
+                if (formPhase > 0) {
+                  setFormPhase(formPhase - 1);
+                }
+              }}
+              className="h-auto rounded-full lg:self-start"
+            >
+              <ChevronLeft className="w-4 h-4" />
+            </Button>
+          )}
+        </div>
+
         {/* Phase 0: Enter Address to send funds to */}
         {formPhase === 0 && enterAddressOption === null && (
           <>
@@ -155,6 +207,7 @@ export default function SendPage() {
             </div>
           </>
         )}
+
         {/* Phase 0 Option A: Wallet Address */}
         {formPhase === 0 && enterAddressOption === "address" && (
           <div className="w-5/6 flex flex-col items-center justify-center">
@@ -179,23 +232,9 @@ export default function SendPage() {
               </div>
               <Button
                 onClick={() => {
-                  if (!addressToPay) {
-                    setAddressToPayError("Please enter an address");
-                    return;
-                  }
-
-                  if (addressToPay === address) {
-                    setAddressToPayError("You can't pay yourself");
-                    return;
-                  }
-
-                  if (addressToPay.length !== 42) {
-                    setAddressToPayError("Invalid address");
-                    return;
-                  }
-
-                  if (!addressToPay.startsWith("0x")) {
-                    setAddressToPayError("Invalid address");
+                  const validation = validateAddress(addressToPay);
+                  if (validation !== true) {
+                    setAddressToPayError(validation);
                     return;
                   }
 
@@ -210,9 +249,84 @@ export default function SendPage() {
           </div>
         )}
 
-        {/* Phase 0 Option B: QR Code */}
+        {/* Phase 0 Option B: Lens Profile */}
+        {formPhase === 0 && enterAddressOption === "lens" && (
+          <>
+            <div className="w-5/6 flex flex-col items-center justify-center">
+              <div className="w-full flex flex-row justify-end items-start gap-2">
+                <div className="w-full flex flex-col">
+                  <Input
+                    type="text"
+                    placeholder="Enter a Lens handle, e.g. jarrodwatts.lens"
+                    className={`w-full`}
+                    value={lensProfileToPay}
+                    onChange={(e) => {
+                      setLensProfileToPay(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
 
-        {/* Phase 0 Option C: Lens Profile */}
+            <div className="w-full flex flex-col mt-8">
+              <h4 className="scroll-m-20 text-lg font-semibold tracking-tight">
+                People
+              </h4>
+
+              <Separator />
+
+              {debouncedLensProfile &&
+                debouncedLensProfile === lensProfileToPay && (
+                  <div className="w-full flex flex-col justify-center items-center gap-2 mt-4">
+                    <div className="w-full flex flex-col gap-2">
+                      {profiles?.map((profile) => (
+                        <Card
+                          onClick={() => {
+                            setAddressToPay(profile.ownedBy.address);
+                            setSelectedLensProfile(profile);
+                            setFormPhase(formPhase + 1);
+                          }}
+                          key={profile.id}
+                          className="w-full flex flex-row items-center justify-start gap-2 py-2 px-3 hover:shadow-lg transition-shadow duration-200 ease-in-out cursor-pointer"
+                        >
+                          <MediaRenderer
+                            // @ts-expect-error: IDK why it doesn't like this
+                            src={
+                              profile?.metadata?.picture?.optimized?.uri ||
+                              `/profile.png`
+                            }
+                            style={{
+                              width: 32,
+                              height: 32,
+                            }}
+                            alt="Lens Profile Avatar"
+                            className="rounded-full"
+                          />
+
+                          <div className="flex flex-col items-start justify-center">
+                            <p className="text-sm lg:text-md font-semibold">
+                              {profile.handle?.suggestedFormatted.localName}
+                            </p>
+                            <p className="text-xs lg:text-sm text-muted-foreground">
+                              {profile.ownedBy.address}
+                            </p>
+                          </div>
+                        </Card>
+                      ))}
+                    </div>
+                  </div>
+                )}
+
+              {debouncedLensProfile !== lensProfileToPay && (
+                <div className="w-full flex flex-col justify-center items-center gap-2 mt-4">
+                  {Array.from(Array(5).keys()).map((index) => (
+                    <Skeleton className="w-full h-12" key={index} />
+                  ))}
+                </div>
+              )}
+            </div>
+          </>
+        )}
 
         {/* Phase 1: Enter Amount to Send */}
         {formPhase === 1 && (
@@ -321,6 +435,20 @@ export default function SendPage() {
 
                   <p className="text-xs lg:text-sm text-muted-foreground">to</p>
                   <p className="text-xs lg:text-md">{addressToPay}</p>
+
+                  {selectedLensProfile && (
+                    <>
+                      <p className="text-xs lg:text-sm text-muted-foreground">
+                        the owner of Lens profile
+                      </p>
+                      <p className="text-xs lg:text-md">
+                        {
+                          selectedLensProfile?.handle?.suggestedFormatted
+                            .localName
+                        }
+                      </p>
+                    </>
+                  )}
                 </div>
               </div>
 
